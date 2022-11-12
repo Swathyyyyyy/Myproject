da8640a81d85f269300152855c0c52b9c64d6a76
Swathyyyyyy <117941741+Swathyyyyyy@users.noreply.github.com>
2022-11-13 01:07:32 +0400

    Update new.py

--- new.py
+++ new.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
+import sys
 
 username = input('Enter your username: ')
 password = input('Enter your password: ')
+
 driver = webdriver.Chrome("chromedriver")
+
 driver.get(" https://www.saucedemo.com/")
 uname = driver.find_element("id", "user-name") 
 uname.send_keys(username)
@@ -15,21 +18,27 @@ driver.find_element("name", "login-button").click()
 WebDriverWait(driver=driver, timeout=10).until(
     lambda x: x.execute_script("return document.readyState === 'complete'")
 )
-error_message = "Incorrect username or password."
-# Retrieve any errors found. 
-errors = driver.find_elements(By.CLASS_NAME, "flash-error")
-if any(error_message in e.text for e in errors): 
-    print("[!] Login failed")
+
+actualUrl="https://www.saucedemo.com/inventory.html" 
+expectedUrl= driver.current_url
+if expectedUrl == actualUrl:
+        print("Login successful")
 else:
-    print("[+] Login successful")
+        print("[!] Login failed")
+        driver.close()
+        sys.exit()
+        
 driver.maximize_window()
 sleep(5)
-#Adding items to cart
 driver.find_element("name","add-to-cart-sauce-labs-backpack").click()
 driver.find_element("name","add-to-cart-sauce-labs-bike-light").click()
 driver.find_element("name","add-to-cart-sauce-labs-bolt-t-shirt").click()
-error_message = "Incorrect username or password."
-errors = driver.find_elements(By.CLASS_NAME, "flash-error")
+
+
+
+    
+
+
 
     
 
