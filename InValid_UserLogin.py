9e43d5bf3a4404381f4e951d2d6b14ebead266c1
Swathyyyyyy <117941741+Swathyyyyyy@users.noreply.github.com>
2022-11-14 15:33:40 +0400

    Update InValid_UserLogin.py

--- InValid_UserLogin.py
+++ InValid_UserLogin.py
@@ -7,17 +7,6 @@ import sys
 username = input('Enter your username: ')
 password = input('Enter your password: ')
 driver = webdriver.Chrome("chromedriver")
-def test_valid_crentials_login(driver):
-    driver.get('http://www.saucedemo.com/')
-
-    driver.find_element(By.ID, 'user-name').send_keys(username)
-    driver.find_element(By.ID, 'password').send_keys(password)
-    driver.find_element(By.ID, 'login-button').click()
-
-    if  "https://www.saucedemo.com/inventory.html" in  driver.current_url:
-               print("Login successful")
-    else:
-                print("[!] Login failed")
             
 def test_invalid_crentials_login(driver):
     driver.get('http://www.saucedemo.com')
